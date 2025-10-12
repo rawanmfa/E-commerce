6d4517eb8e4615d8c098a3d9727f9d6736b51c96
rawanmfa <rawanmahmoudrm763711@gmail.com>
2025-10-12 03:23:43 +0300

    Replaced project files with final e-commerce version

--- src/Components/AllOrders/AllOrders.jsx
+++ src/Components/AllOrders/AllOrders.jsx
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { cartContext } from '../../Context/cartContext'
+import ThankYouPage from '../../assets/images/Thank-You-Page.png'
 
 export default function AllOrders() {
 
-    let{deleteCart} = useContext(cartContext);
-    useEffect(()=>{
-        deleteCart()
-    },[])
-    
+  let { deleteCart } = useContext(cartContext);
+  useEffect(() => {
+    deleteCart()
+  }, [])
+
   return <>
-    
-    <h1 className="text-3xl mt-8">AllOrders</h1>
-  
+
+    <div className=' my-10'>
+      <p className=' text-green-700 md:text-lg mt-20 mb-5 text-center'>Thank you for using FreshCart your order will be shiped soon 🎉🎉🎉</p>
+      <img src={ThankYouPage} alt="Thank-You-Page" className=' w-3/4 m-auto lg:h-[500px]' />
+    </div>
   </>
 }
