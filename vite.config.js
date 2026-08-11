d0d37b922ab5883eef96b1afb3d87b9b415e8278
rawanmfa <rawanmahmoudrm763711@gmail.com>
2026-08-11 03:20:01 +0300

    updating the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/E-commerce/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 })
